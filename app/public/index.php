4265f49786a0e7f4649dbb2d38786578d3fa9e11
Evgeny <skchel174@gmail.com>
2022-11-21 19:33:53 +0500

    installed symfony http-foundation

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-phpinfo();
+declare(strict_types=1);
+
+use Symfony\Component\HttpFoundation\Request;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+dd(Request::createFromGlobals());
