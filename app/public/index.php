951b9bb72ce68045b4014d2b3384c88069ad3e25
Evgeny Kuzmin <skchel174@gmail.com>
2022-11-27 15:58:48 +0500

    Created RouterFactory

--- app/public/index.php
+++ app/public/index.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use App\Framework\Container\ContainerFactory;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Router;
 
@@ -18,31 +16,23 @@ const DEBUG = true;
 require_once BASE_DIR . '/vendor/autoload.php';
 
 try {
-    $containerFactory = new ContainerFactory(BASE_DIR);
-    $container = $containerFactory(DEBUG);
-
-    $configLocator = new FileLocator(BASE_DIR . '/config');
+    $containerFactory = new ContainerFactory(DEBUG);
+    $container = $containerFactory();
 
     // Initialize Router
     $requestContext = new RequestContext();
     $requestContext->fromRequest($request = Request::createFromGlobals());
-
-    $routerCache = !DEBUG ? BASE_DIR . '/var/cache' : null;
-
-    $router = new Router(
-        new YamlFileLoader($configLocator),
-        'routes.yaml',
-        ['cache_dir' => $routerCache],
-        $requestContext,
-    );
+    /** @var Router $router */
+    $router = $container->get(Router::class);
+    $router->setContext($requestContext);
 
     // Routing
     $parameters = $router->match($requestContext->getPathInfo());
     $request->attributes->add($parameters);
-
     [$controllerName, $method] = explode('::', $parameters['_controller']);
     $controller = $container->get($controllerName);
 
+    // Call handler
     /** @var Response $response */
     $response = $controller->$method($request);
     echo $response->getContent();
