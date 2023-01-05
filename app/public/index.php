54f59e165baeb0619010f377b09a3be268c8fdf1
Evgeny Kuzmin <skchel174@gmail.com>
2023-01-05 16:12:19 +0500

    Added APP_DEBUG resolving to index.php

--- app/public/index.php
+++ app/public/index.php
@@ -12,9 +12,13 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $envManager = new Dotenv();
 $envManager->loadEnv(dirname(__DIR__) . '/.env');
+if (!isset($_ENV['APP_DEBUG'])) {
+    $_ENV['APP_DEBUG'] = $_SERVER['APP_DEBUG'] = $_ENV['APP_ENV'] === 'prod' ? 0 : 1;
+}
 
-ErrorHandler::register(new ErrorHandler(new BufferingLogger(), (bool)$_SERVER['APP_DEBUG']));
+$handler = new ErrorHandler(new BufferingLogger());
+ErrorHandler::register($handler, (bool)$_ENV['APP_DEBUG']);
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+$kernel = new Kernel($_ENV['APP_ENV'], (bool)$_ENV['APP_DEBUG']);
 $response = $kernel->handle(Request::createFromGlobals());
 $response->send();
