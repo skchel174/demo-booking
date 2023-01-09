1dbf9e1bb5c045ac76c005b020225e40b28e6067
Evgeny Kuzmin <skchel174@gmail.com>
2023-01-09 22:40:03 +0500

    Updated RouterFactory, added RoutesResource

--- app/framework/src/Router/RouterFactory.php
+++ app/framework/src/Router/RouterFactory.php
@@ -2,19 +2,26 @@
 
 namespace Framework\Router;
 
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\Router;
 
 class RouterFactory
 {
+    public function __construct(
+        private readonly LoaderInterface $loader,
+        private readonly mixed $resource
+    )
+    {
+    }
+
+    /**
+     * @param Container $container
+     * @return Router
+     */
     public function __invoke(Container $container): Router
     {
-        $locator = new FileLocator($container->getParameter('kernel.config_dir'));
-        $loader = new YamlFileLoader($locator);
-
-        return new Router($loader, 'routes.yaml', [
+        return new Router($this->loader, $this->resource, [
             'debug' => $container->getParameter('kernel.debug'),
             'cache_dir' => $container->getParameter('kernel.cache_dir'),
         ]);
